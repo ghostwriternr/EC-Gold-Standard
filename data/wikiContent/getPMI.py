83be79444f152a50821a33d98e24d35d201b3115
Naresh <ghostwriternr@gmail.com>
2018-02-17 17:02:26 +0530

    Add getpmi method

---
+++ data/wikiContent/getPMI.py
@@ -0,0 +1,48 @@
+import os
+import json
+from math import log
+
+DIRECTORIES = ['./animalsJson', './plantsJson']
+
+def split_sentences(data_json):
+    all_sentences = ''
+    for key in data_json:
+        all_sentences += data_json[key].strip()
+    sentence_list = all_sentences.split(".")
+    for sentence in sentence_list:
+        sentence = sentence.strip()
+        sentence = sentence.replace('\n', '')
+    return sentence_list
+
+def getcooccurence(str1, str2, sentence_list, scores):
+    for sentence in sentence_list:
+        if str1 in sentence:
+            scores['first'] += 1
+        if str2 in sentence:
+            scores['second'] += 1
+        if str1 in sentence and str2 in sentence:
+            scores['both'] += 1
+
+def calculatepmi(scores):
+    return log(scores['both']/(scores['first']*scores['second']))
+
+def getpmi(str1, str2):
+    scores = {
+        'both': 0,
+        'first': 0,
+        'second': 0
+    }
+    for directory in DIRECTORIES:
+        for file in os.listdir(directory):
+            if file.endswith(".json"):
+                with open(os.path.join(directory, file)) as data_file:
+                    data = json.load(data_file)
+                sentence_list = split_sentences(data)
+                getcooccurence(str1, str2, sentence_list, scores)
+    return calculatepmi(scores)
+
+def main():
+    print(getpmi('the', 'a'))
+
+if __name__ == "__main__":
+    main()
